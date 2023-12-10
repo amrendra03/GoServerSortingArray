f1cf6a576c67a8b7a638d1f3a963ed2dde394f97
amrendra03 <amrendra9076893685@gmail.com>
2023-12-10 12:00:16 +0530

    creating-fun-process-single

--- api/main.go
+++ api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,36 @@ func main() {
 	fmt.Printf("Server running on port: %d", port)
 
 	router.Run(fmt.Sprintf(":%d", port))
+}
+
+// ---------process single sequentially---
+func processSingle(c *gin.Context) {
+
+	var req SortRequest
+
+	// c.ShouldBindJSON to bind the json data in user struct data formate
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+
+	// performing sorting
+	sortedArrays := make([][]int, len(req.ToSort))
+	for i, arr := range req.ToSort {
+
+		sortedArr := make([]int, len(arr))
+		copy(sortedArr, arr)
+		sort.Ints(sortedArr)
+		sortedArrays[i] = sortedArr
+	}
+
+	// bind result into struct
+	res := SortResponse{
+		SortedArrays: sortedArrays,
+		TimeNS:       0,
+	}
+
+	// response struct send in response in JSON formate
+	c.JSON(http.StatusOK, res)
+
 }
